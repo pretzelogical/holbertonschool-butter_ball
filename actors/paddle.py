5a7383107f57464d5429145d0d0797499e5803db
Kolton Rhodes <gamerlife467@gmail.com>
2023-08-19 11:21:30 -0500

    paddletostop

--- actors/paddle.py
+++ actors/paddle.py
@@ -13,7 +13,6 @@ class Paddle():
             initPos: position to start paddle in new round
             moveSpeed: multiplied against deltatime to get movement
             rect: rectanglular representation of the paddle
-            new_x: stops the paddle from leaven screen
     """
     color = pygame.Color('white')
 
@@ -33,22 +32,6 @@ class Paddle():
             self.rect.x += self.moveSpeed * self.__game.deltaTime
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.moveSpeed * self.__game.deltaTime
-
-    def updatePos(self):
-        keys = self.__game.getKeys()
-
-        # Calculate the new position based on user input and movement speed
-        new_x = self.rect.x
-        if keys[pygame.K_RIGHT]:
-        new_x += self.moveSpeed * self.__game.deltaTime
-        if keys[pygame.K_LEFT]:
-             new_x -= self.moveSpeed * self.__game.deltaTime
-
-        # Apply boundary constraints to prevent the paddle from moving off-screen horizontally
-        new_x = max(0, min(new_x, self.__game.scSize[0] - self.width))
-
-        # Update the paddle's horizontal position
-        self.rect.x = new_x
 
     def draw(self):
         pygame.draw.rect(self.__game.screen, self.color, self.rect)
